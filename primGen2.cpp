607863270d0441298cd93e4ccebc1d805cfe7f12
Richard Zeitler <ciatronical@lxcars.de>
2018-10-22 09:01:26 +0200

    Die letzten änderungen  beendet

--- primGen2.cpp
+++ primGen2.cpp
@@ -4,49 +4,38 @@
 using namespace std;
  
 int main(){
-    int upper = 18;
-    int lower = 7;
-    int div = 2;
-    int result = 0;
-    int counter = 0;
+    int upper = 18;                                                 // obere grenze
+    int lower = 7;                                                  // untere grenze
+    int div = 2;                                                    // durch diese zahl wird dividiert
+    int result = 0;                                                 // der modulo wert (division mit rest)
+    int counter = 0;                                                // variable die die nullen bei der modulo rechnung zählt
 
-    cout << "upper limit:";
-    cin >> upper;    
-    cout << "lower limit:";
-    cin >> lower;
-    
+    cout << "untere grenze :";                                         // gibt dem user die nachicht "upper limit"
+    cin >> upper;                                                   // der user input wird als "upper" variable deffiniert   
+    cout << "obere grenze:";                                         // giebt dem uer die nachicht "lower limit"
+    cin >> lower;                                                   // der userinput wird als "lower" deffiniert
     //cout << "test" << endl;
-
-    for( int i = lower; i < upper; i++ ){
-
-        //cout << i << endl;
-
-        while( div < i ){
-
-            result = i % div;
+ 
+    for( int i = lower; i < upper; i++ ){                           /*Zählen von der unterren grenze bis zur oberen grenze ( for muss haben!!!!)
+        //cout << i << endl;                                   1. stelle neue variable 2. stelle laufbedingung 3.stelle was am ende ausgefürt werden soll*/
+        while( div < i ){                                           // schleife die läuft wenn der divident kleiner als die unterre grenze ist 
+            result = i % div;                                       // ergebniss
              //cout << "c ist: " << result << endl;
-
             if( result == 0 ){
                 counter++;
                 //cout << "anzahl der nullen:" << counter << endl;
             }           
             div++;
-            
-                
         }//end while
 
         if( counter == 0 ){
-        cout << i << " ist eine Primzahl:" << endl; 
+        cout << i <<";";                  //schaut ob die eigenschaften einer primzahl erfüllt sind
         }
-
-        if( counter != 0 ){
+        /*if( counter != 0 ){                                          //
         cout << i << " ist keine Primzahl:" << endl;
-        }
-
+        }*/
         counter = 0;
         div = 2;
-        
-        
     }
     return 0;
 }
